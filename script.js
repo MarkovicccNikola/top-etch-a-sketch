11c472322dab21003817702736e0735e16b26a34
Nikola <markovi3c.nikola@gmail.com>
2023-01-25 10:30:19 +0100

    Add transition when any part of the grid is hovered

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const container = document.querySelector("#container");
 for (i = 0; i < 256; i++) {
   const gridSquare = document.createElement("div");
   gridSquare.setAttribute("id", "element");
-  gridSquare.textContent = `${i}`;
+  //gridSquare.textContent = `${i}`;
   container.appendChild(gridSquare);
 }
