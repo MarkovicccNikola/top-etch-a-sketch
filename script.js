7510df188fcaf34f846bd69210be35067727b066
Nikola <markovi3c.nikola@gmail.com>
2023-01-25 15:18:35 +0100

    Set an initial grid instead of having empty container

--- script.js
+++ script.js
@@ -2,6 +2,19 @@ const container = document.querySelector("#container");
 const buttonGridChange = document.querySelector("button");
 let gridSize = null;
 
+function makeInitialGrid() {
+  let defaultSize = 800 / 4;
+  for (i = 0; i < 4 * 4; i++) {
+    const gridSquare = document.createElement("div");
+    gridSquare.style.width = defaultSize + "px";
+    gridSquare.style.height = defaultSize + "px";
+    gridSquare.setAttribute("id", "element");
+    container.appendChild(gridSquare);
+  }
+}
+
+makeInitialGrid();
+
 buttonGridChange.addEventListener("click", () => {
   gridSize = prompt("Enter the size of the grid");
   if (gridSize > 100) {
@@ -9,6 +22,8 @@ buttonGridChange.addEventListener("click", () => {
   }
   makeGrid();
 });
+
+//I could also get the value of width and height of the container if I am using %s and then use that instead of hard coded size as I did here.
 
 function makeGrid() {
   container.innerHTML = "";
