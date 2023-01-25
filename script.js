774ab26cdd32a077eb58254ba0d3b1fa2be86397
Nikola Markovic <markovi3c.nikola@gmail.com>
2023-01-25 14:13:09 +0100

    Add a button to ask for user's desired grid size. Remove current grid and make a new one.

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const container = document.querySelector("#container");
+const buttonGridChange = document.querySelector('button');
+let gridSize = null;
 
-for (i = 0; i < 256; i++) {
-  const gridSquare = document.createElement("div");
-  gridSquare.setAttribute("id", "element");
-  //gridSquare.textContent = `${i}`;
-  container.appendChild(gridSquare);
+buttonGridChange.addEventListener('click', () => {
+  gridSize = prompt('Enter the size of the grid');
+  if (gridSize > 100) {
+    gridSize = prompt(`Don't enter a value above 100`);
+  }
+  makeGrid();
+});
+
+function makeGrid() {
+  container.innerHTML = '';
+  for (i = 0; i < gridSize * gridSize; i++) {
+    const gridSquare = document.createElement('div');
+    gridSquare.setAttribute('id', 'element');
+    //gridSquare.textContent = `${i}`;
+    container.appendChild(gridSquare);
+  }
 }
+
